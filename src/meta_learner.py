fadf8fcf9e32e4de2e2089667cb4f9a753640545
Creator-Codie-afk <Ibecodie@gmail.com>
2025-01-31 07:47:25 -0800

    Update meta_learner.py

--- src/meta_learner.py
+++ src/meta_learner.py
@@ -5,14 +5,33 @@ class MetaLearner(BaseEstimator, ClassifierMixin):
         self.base_model = base_model
 
     def fit(self, X, y):
-        """Train the meta-learning model."""
+
+
+    Parameters:
+    X (pd.DataFrame): The training data.
+    y (pd.Series): The training labels.
+
+    Returns:
+    self: Returns the instance of the MetaLearner.
+    """
         self.base_model.fit(X, y)
         return self
 
     def predict(self, X):
         """Predict using the meta-learning model."""
+
+    Parameters:
+    X (pd.DataFrame): The data to predict.
+
+    Returns:
+    np.ndarray: The predicted labels.
+    """
         return self.base_model.predict(X)
 
     def adapt(self, new_data, new_labels):
-        """Adapt the meta-learning model to new data."""
+        """Adapt the meta-learning model to new data.""""""Adapt the meta-learning model to new data.
+
+    Parameters:
+
+    """
         self.base_model.fit(new_data, new_labels)
