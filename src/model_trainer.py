748e29eb1e1dc8ec91ad676696607e5d512fbcfe
Creator-Codie-afk <Ibecodie@gmail.com>
2025-01-31 07:48:46 -0800

    Update model_trainer.py

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -2,11 +2,24 @@ from sklearn.ensemble import RandomForestClassifier
 from joblib import dump
 
 def train_model(data, labels):
-    """Train a machine learning model using RandomForestClassifier."""
+   "
+
+    Parameters:
+    data (pd.DataFrame): The training data.
+    labels (pd.Series): The training labels.
+
+    Returns:
+    RandomForestClassifier: The trained model.
+    """"""
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(data, labels)
     return model
 
 def save_model(model, filepath):
-    """Save the trained model to a file using joblib."""
+   
+
+    Parameters:
+    model: The trained machine learning model.
+    filepath (str): The path to the file where the model will be saved.
+    """
     dump(model, filepath)
